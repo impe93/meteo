0aaf819e37325693bc4210ba59d5a558721f35ca
Lorenzo Imperatrice <lorenzo.imperatrice@ovalmoney.com>
2021-04-01 23:12:08 +0200

    Add usePosition hook

---
+++ src/hooks/usePosition.ts
@@ -0,0 +1,54 @@
+import { useState, useEffect } from 'react';
+
+type Settings = {
+  enableHighAccuracy: boolean,
+  timeout: number,
+  maximumAge: number,
+}
+
+const defaultSettings: Settings = {
+  enableHighAccuracy: false,
+  timeout: Infinity,
+  maximumAge: 0,
+};
+
+export const usePosition = (watch = false, settings: Settings = defaultSettings) => {
+  const [position, setPosition] = useState({});
+  const [error, setError] = useState(null);
+
+  const onChange = ({ coords, timestamp }) => {
+    setPosition({
+      latitude: coords.latitude,
+      longitude: coords.longitude,
+      accuracy: coords.accuracy,
+      speed: coords.speed,
+      timestamp,
+    });
+  };
+
+  const onError = (error) => {
+    setError(error.message);
+  };
+
+  useEffect(() => {
+    if (!navigator || !navigator.geolocation) {
+      setError('Geolocation is not supported');
+      return;
+    }
+
+    let watcher = null;
+    if (watch) {
+      watcher = navigator.geolocation.watchPosition(
+        onChange,
+        onError,
+        settings
+      );
+    } else {
+      navigator.geolocation.getCurrentPosition(onChange, onError, settings);
+    }
+
+    return () => watcher && navigator.geolocation.clearWatch(watcher);
+  }, [settings.enableHighAccuracy, settings.timeout, settings.maximumAge]);
+
+  return { ...position, error };
+};
