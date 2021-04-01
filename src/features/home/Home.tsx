3aceeb736166b514507e4b457ed17c7d3be9c2c8
Lorenzo Imperatrice <lorenzo.imperatrice@ovalmoney.com>
2021-04-01 22:53:05 +0200

    Add Home UI

--- src/features/home/Home.tsx
+++ src/features/home/Home.tsx
@@ -1,9 +1,38 @@
 import React from 'react';
+import styleSheet from './home.module.css';
+import WeatherIcon from 'react-icons-weather';
 
-type Props = {
-
-}
+type Props = {};
 
 export const Home = () => {
-  return (<div></div>)
-}
+
+
+  return (
+    <div className={styleSheet.Home}>
+      <div className={styleSheet.verticalContainer}>
+        <div className={styleSheet.horizontalContainer}>
+          <WeatherIcon
+            className={`${styleSheet.fontSize} ${styleSheet.shadow}`}
+            name='owm'
+            flip="horizontal"
+            iconId='500'
+          />
+          <div
+            className={`${styleSheet.fontSize} ${styleSheet.shadow} ${styleSheet.text}`}
+          >
+            14°C
+          </div>
+        </div>
+        <div className={`${styleSheet.shadow} ${styleSheet.description}`}>
+          Oggi a <b>Torino</b> c'è <b>pioggia moderata</b>
+        </div>
+        <div className={`${styleSheet.shadow} ${styleSheet.update}`}>
+          Ultimo aggiornamento 24-10-2019, 11:31:29
+        </div>
+        <div className={`${styleSheet.shadow} ${styleSheet.update}`}>
+          Powered by <b>Lorenzo Imperatrice</b>
+        </div>
+      </div>
+    </div>
+  );
+};
