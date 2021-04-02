0f2891857f0887d0e09fe9849fd50fa2182252e8
Lorenzo Imperatrice <lorenzo.imperatrice@ovalmoney.com>
2021-04-02 10:59:49 +0200

    Add correct weather

--- src/features/home/Home.tsx
+++ src/features/home/Home.tsx
@@ -1,30 +1,44 @@
 import React from 'react';
 import styleSheet from './home.module.css';
 import WeatherIcon from 'react-icons-weather';
+import { useSelector } from 'react-redux';
+import { selectIsLoading } from '../../components/Loader/loaderSlice';
+import { Loader } from '../../components/Loader/Loader';
+import { Weather } from './weather.models';
+import { selectWeather } from './homeSlice';
 
 type Props = {};
 
 export const Home = () => {
+  const isLoading: boolean = useSelector(selectIsLoading);
+  const weather: Weather = useSelector(selectWeather);
+
+  const temperature = weather.main.temp.toFixed(0);
+  const iconId = String(weather.weather[0].id);
+  const city = weather.name;
+  const weatherDescription = weather.weather[0].description;
 
 
-  return (
+  return isLoading || !weather ? (
+    <Loader />
+  ) : (
     <div className={styleSheet.Home}>
       <div className={styleSheet.verticalContainer}>
         <div className={styleSheet.horizontalContainer}>
           <WeatherIcon
             className={`${styleSheet.fontSize} ${styleSheet.shadow}`}
             name='owm'
-            flip="horizontal"
-            iconId='500'
+            flip='horizontal'
+            iconId={iconId}
           />
           <div
             className={`${styleSheet.fontSize} ${styleSheet.shadow} ${styleSheet.text}`}
           >
-            14°C
+            {temperature}°C
           </div>
         </div>
         <div className={`${styleSheet.shadow} ${styleSheet.description}`}>
-          Oggi a <b>Torino</b> c'è <b>pioggia moderata</b>
+          Oggi a <b>{city}</b> c'è <b>{weatherDescription}</b>
         </div>
         <div className={`${styleSheet.shadow} ${styleSheet.update}`}>
           Ultimo aggiornamento 24-10-2019, 11:31:29
