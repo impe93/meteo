e5a0e907a23b8c859490d9efbb1ec727bc18dce5
Lorenzo Imperatrice <lorenzo.imperatrice@ovalmoney.com>
2021-04-02 12:50:07 +0200

    Get forecast from component Forecast

--- src/features/forecast/Forecast.tsx
+++ src/features/forecast/Forecast.tsx
@@ -1,5 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { IHttpClient, IHttpClientType } from '../../services/http/HttpClient';
+import { useDependency } from '../../services/ioc/useDependency';
+import { selectPosition } from '../home/homeSlice';
+import { getForecast, selectForecast } from './forecastSlice';
 
 export const Forecast = () => {
+  const dispatch = useDispatch();
+  const forecast = useSelector(selectForecast);
+  const position = useSelector(selectPosition);
+  const httpClient = useDependency<IHttpClient>(IHttpClientType);
+
+  useEffect(() => {
+    if (position) {
+      dispatch(getForecast(position, httpClient))
+    }
+  }, [position, httpClient, dispatch])
+
   return (<div></div>)
 }
